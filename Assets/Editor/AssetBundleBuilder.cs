a29f2a75f59362a43d97122786bd7546fdc18d68
city0409 <250924125@qq.com>
2018-02-28 17:44:11 +0800

    Initial commit

---
+++ Assets/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetBundleBuilder : Editor 
+{
+
+	[MenuItem("Assets/Create/Build Asset bundle")]
+    static void BuildAB()
+    {
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        AssetDatabase.Refresh();
+        Debug.Log("finish");
+    }
+}
