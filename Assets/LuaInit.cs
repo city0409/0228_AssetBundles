ee83713eeb2d17be0edea424a2ff67f1f8fb1a03
city0409 <250924125@qq.com>
2018-03-08 11:56:37 +0800

    0307PM，~End~

---
+++ Assets/LuaInit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+using System.IO;
+
+[Hotfix]
+public class LuaInit : MonoBehaviour 
+{
+    private static LuaEnv luaEnv;
+
+	private void Start() 
+	{
+        luaEnv = new LuaEnv();
+
+    }
+	
+	private void Update () 
+	{
+        //print("print from c#");
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            InitAllLuaScripts();
+        }
+	}
+
+    private void InitAllLuaScripts()
+    {
+        string luaScriptsPath = Path.Combine(AppConst.DataPath, "LuaScripts");
+
+        string[] fileNames = Directory.GetFiles(luaScriptsPath);
+
+        for (int i = 0; i < fileNames.Length; i++)
+        {
+            byte[] bytes = File.ReadAllBytes(fileNames[i]);
+            luaEnv.DoString(System.Text.Encoding.UTF8.GetString(bytes));
+        }
+        print("All lua injected");
+    }
+}
---
+++ Assets/TestCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+[Hotfix]
+public class TestCube : MonoBehaviour 
+{
+
+	private void Awake() 
+	{
+		
+	}
+	
+	private void Update () 
+	{
+        if (Input .GetKeyDown(KeyCode.Q))
+        {
+            print("c# QQQQQQQQQQQQQQQQQQQ");
+        }
+	}
+}
